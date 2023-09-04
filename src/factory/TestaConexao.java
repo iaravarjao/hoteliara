63dfa8ce5f8870c958abbf5a795ec75b89a98f0b
Iara Varjao Barbosa <103008910+iaravarjao@users.noreply.github.com>
2023-09-04 18:36:40 -0300

    Add files via upload

---
+++ src/factory/TestaConexao.java
@@ -0,0 +1,19 @@
+package factory;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao();
+
+		System.out.println("Conexao aberta!! ");
+
+		connection.close();
+	}
+
+}
